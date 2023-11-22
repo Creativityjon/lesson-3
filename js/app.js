6f172f32d9175912983c62b19d3989778861be6a
Muhammad Yusuf Hamidov <muhammadyusufhamidov@Muhammads-MacBook-Air.local>
2023-11-22 11:35:48 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+// const a = 0.1;
+// const b = 0.2;
+// const c = 0.3;
+// console.log(a + b === c);
+
+/* 6 - example
+
+let a = +prompt("son kiriting !");
+let b = +prompt("son kiriting !");
+
+if (a > b) {
+  console.log(`kotta son:` + " " + a);
+} else if (b > a) {
+  console.log(`kotta son` + " " + b);
+} else if (a === b) {
+  console.log(`ikkita son teng:` + " " + a,b);
+}
+
+*/
+
+/* 7 - example
+
+let a = +prompt('son kiriting:');
+let b = +prompt('son kiriting:');
+
+if (a < b) {
+  console.log(`kichik son:` + " " + a);
+} else if (b < a) {
+  console.log(`kichik son:` + " " + b);
+} else if (a === b) {
+  console.log(`ikkita son teng`);
+}
+
+*/
+
+/* 8 - example
+
+let a = +prompt("son kiriting");
+let b = +prompt("son kiriting");
+
+if (a > b) {
+  console.log(`kotta son:` + ' ' + a);
+} else if (b > a) {
+  console.log(`kotta son:` + ' ' + b);
+} else if (a == b) {
+  console.log(`ikkita son teng`);
+}
+
+if (a < b) {
+  console.log(`kichik son:` + ' ' + a);
+} else if (b < a) {
+  console.log(`kichik son:` + ' ' + b);
+} else if (a == b) {
+  console.log(`ikki son teng`);
+}
+
+*/
+
